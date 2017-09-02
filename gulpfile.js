057485deea8ad736f1751c7706868d95355f0833
George Roberts <george.roberts@matchesfashion.com>
2017-09-02 20:41:45 +0100

    added linting, gulp and es6 transpilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 const gulp = require('gulp');
-//gulp depedencies go here
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
-gulp.task('default', function(){
-    //gulp tasks go here
+gulp.task('default', function() {
+   //Run ESLint
+   gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+       .pipe(eslint())
+       .pipe(eslint.format());
+   //node source
+   gulp.src("es6/**/*.js")
+       .pipe(babel())
+       .pipe(gulp.dest("dist"));
+   //browser source
+   gulp.src("public/es6/**/*.js")
+       .pipe(babel())
+       .pipe(gulp.dest("public/dist"));
 });
-
